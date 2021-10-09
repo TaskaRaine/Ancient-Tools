e7d7f21c67f59f1574895cd68b37e68b679b4e66
Taska Raine <taskaraine@gmail.com>
2021-10-09 12:34:35 -0400

    1.2.4 Fixed
    Forgot to multiply the output...

--- src/items/ItemBark.cs
+++ src/items/ItemBark.cs
@@ -6,7 +6,18 @@ namespace AncientTools.Items
     {
         public override void OnCreatedByCrafting(ItemSlot[] allInputslots, ItemSlot outputSlot, GridRecipe byRecipe)
         {
-            outputSlot.Itemstack.StackSize = api.World.Config.GetInt("BarkPerLog", 4);
+            int logCount = 1;
+
+            foreach(ItemSlot slot in allInputslots)
+            {
+                if (slot.Empty)
+                    continue;
+
+                if (slot.Itemstack.Collectible.FirstCodePart() == "log")
+                    logCount = slot.Itemstack.StackSize;
+            }
+
+            outputSlot.Itemstack.StackSize = api.World.Config.GetInt("BarkPerLog", 4) * logCount;
 
             base.OnCreatedByCrafting(allInputslots, outputSlot, byRecipe);
         }
